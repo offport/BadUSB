3bfe13837eaf55b407b030c7bb286012863b689e
offport <93790223+offport@users.noreply.github.com>
2022-01-01 23:18:28 -0800

    Update PlaySound.ps1

--- Windows/Payloads/PlaySound.ps1
+++ Windows/Payloads/PlaySound.ps1
@@ -1,3 +1,3 @@
 Function Speak-Text($Text) { Add-Type -AssemblyName System.speech; $TTS = New-Object System.Speech.Synthesis.SpeechSynthesizer; $TTS.Speak($Text) }
-Speak-Text "Taybe. Taybe. Jiba."
+Speak-Text "You have been hacked. Now you have to do everything we ask you."
 Exit
